371b14103b050ca24f75f76ece7ba2ff56d68626
Cesar Casares <cacjmuse@gmail.com>
2025-03-14 20:40:52 -0500

    feat: setting up react query, fecthin data, handling errors y showing a loading indicator

---
+++ src/react-query/TodoList.tsx
@@ -0,0 +1,35 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+
+interface Todo {
+    id: number;
+    title: string;
+}
+
+const TodoList = () => {
+    const fetchTodos = () =>
+        axios
+        .get<Todo[]>('https://jsonplaceholder.typicode.com/todos')
+        .then(res => res.data)
+
+    const {data: todos, error, isLoading} = useQuery<Todo[], Error>({
+        queryKey: ['todos'],
+        queryFn: fetchTodos
+           
+    })
+
+    if (isLoading) return <p>Loading...</p>
+    if(error) return <p>{error.message} </p>;
+    return (
+    <ul className='list-group'>
+        {todos?.map((todo) => (
+            <li key={todo.id} className='list-group-item'>
+                {todo.title}
+            </li>
+        ))}
+    </ul>
+  )
+}
+
+export default TodoList
