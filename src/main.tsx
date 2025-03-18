6687f94569d04b3c10545b536e5387e31f771dad
Cesar Casares <cacjmuse@gmail.com>
2025-03-18 18:49:09 -0500

    feat: using react quey devtools customizing query settings

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,26 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-
-const queryClient = new QueryClient();
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 3,
+      gcTime: 300_000,
+      staleTime: 10 * 1000,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      refetchOnMount: false
+    }
+  }
+});
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools />
     </QueryClientProvider>
   </StrictMode>,
 )
